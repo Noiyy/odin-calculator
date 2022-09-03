1160c705a0dfbefd702a8088912898f1cd28b271
Benjamín Muľár <83555059+Noiyy@users.noreply.github.com>
2022-09-03 14:41:48 +0200

    Make display show nums
    Added functions to display numbers on display when they're pressed and
    a simple transition on button hover & click.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+let value = 0;
+let _x = 0;
+let _y = 0;
+let _operator;
+
+const buttons = document.querySelectorAll("button");
+const displayText = document.querySelector(".display p");
+const numButtons = document.querySelectorAll(".numbers button");
+const operatorButtons = document.querySelectorAll(".sidebar button");
+const equalsBtn = document.querySelector("button.equals");
+
 function add(x, y) {
     return x + y;
 }
@@ -17,9 +28,43 @@ function operate(operator, x, y) {
             return add(x, y);
         case "-":
             return subtract(x, y);
-        case "*":
+        case "x":
             return multiply(x, y);
         case "/":
             return divide(x, y);
     }
+}
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", buttonClicked);
+    btn.addEventListener("transitionend", removeTransition);
+});
+numButtons.forEach(btn => {
+    btn.addEventListener("click", clickedNum);
+});
+operatorButtons.forEach(btn => {
+    btn.addEventListener("click", clickedOperator);
+});
+
+function clickedNum(e) {
+    if (this.classList.contains("equals")) return;
+    value = e.srcElement.textContent;
+    if (displayText.textContent == 0)
+        displayText.textContent = value;
+    else
+        displayText.textContent += value;
+}
+
+function clickedOperator(e) {
+    _operator = e.srcElement.textContent;
+    console.log(_operator);
+}
+
+function buttonClicked() {
+    this.classList.add("clicked");
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("clicked");
 }
