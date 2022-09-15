feb32dc2dcd7478c8d5f5e6854101e590787f02a
Benjamín Muľár <83555059+Noiyy@users.noreply.github.com>
2022-09-15 14:09:05 +0200

    Add multi-operations support
    Now, it's able to calculate more operations by evaluating the first one
    and saving it, then doing it again with the next operator & number

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,6 @@ const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
 function operate(operator, x, y) {
-    if (!result) displayPrevText.textContent += "=";
     switch (operator) {
         case "+":
             result = Math.round( add(x, y) * 100) / 100;
@@ -60,7 +59,7 @@ function clickedEquals() {
         displayPrevText.textContent = displayPrevText.textContent.slice(0, displayPrevText.textContent.length);
         return;
     }
-    displayPrevText.textContent += `${_y} `;
+    displayPrevText.textContent += `${_y} =`;
     operate(_operator, _x, _y);
 }
 
@@ -70,17 +69,27 @@ function clickedNum(e) {
     
     value = e.srcElement.textContent.trim();
     let mainText = displayMainText.textContent;
-    if (mainText == 0 && !mainText.includes(".") && value != ".") displayMainText.textContent = value;
+    if (mainText == 0 && !mainText.includes(".") && value != "." || result) displayMainText.textContent = value;
     else displayMainText.textContent += value;
     value = displayMainText.textContent;
 }
 
 function clickedOperator(e) {
-    _operator = e.srcElement.textContent.trim();
-    _x = Number(value);
-    value = 0;
-    displayMainText.textContent = value;
-    displayPrevText.textContent += `${_x} ${_operator} `;
+    if (_operator) {
+        _y = Number(value);
+        operate(_operator, _x, _y);
+        _operator = e.srcElement.textContent.trim();
+        displayPrevText.textContent += `${_y} ${_operator} `;
+        _x = result;
+        value = _y = 0;
+
+    } else {
+        _operator = e.srcElement.textContent.trim();
+        if (_x) _y = Number(value);
+        else _x = Number(value);
+        displayMainText.textContent = value = 0;
+        displayPrevText.textContent += `${_x} ${_operator} `;
+    }
 }
 
 function buttonClicked(e) {
